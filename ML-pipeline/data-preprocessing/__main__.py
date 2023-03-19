aea67b6bd5539ce327cedf1a68748bf5df430e88
anastasiakrouglova <anastasia.krouglova@student.howest.be>
2023-03-19 17:02:29 +0100

    data-preprocessing main file

--- ML-pipeline/data-preprocessing/__main__.py
+++ ML-pipeline/data-preprocessing/__main__.py
@@ -1,24 +1,33 @@
 # Load installed libraries
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import os
 import sys
 
-##########################################################################################
+# Load local libraries
 __file__ = "utilities_store.py"
-ROOT = "../../fpt/"
-DATA = "../../fpt/data/output/flute-a4.csv"
+ROOT = './fpt/'
 
-# path resolving
+# Add path to own library
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), ROOT))
 if not path in sys.path:
-    sys.path.insert(1, path)
+    sys.path.insert(1, path) # Warning: can probably cause errors on other machines since paths were not deleted, run print(sys.path) to examine which paths were created
+
+
 
 import utilities_store as us
+
 ##########################################################################################
 
-# Load csv file for flute
-df = pd.read_csv(DATA)
+DATA = "./fpt/data/output/flute-a4.csv"
+
+# Load csv file of raw data
+raw = pd.read_csv(DATA)
+
+print(os.path.abspath(DATA))
+
+def removeNoiseFromPadding(data):
+    # Noise appears at the beginning and end of the data from padding the signal 
+    # with zeros on the left and right so that the window/step-size/overlap work out.
+    data[(data.onset != 0) & (data.onset != max(data.onset))]
+
+
